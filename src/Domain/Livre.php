9a20c3b50c78f700d08bb805a21815f2111dbe22
edgetahiti <edouard@edge.pf>
2016-02-20 16:49:11 -1000

    Déclaration des nom de la classe livre

---
+++ src/Domain/Livre.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace MyBooks\Domain;
+
+class Livre 
+{
+    /**
+     * Livre id.
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * Livre title.
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Livre summary.
+     *
+     * @var string
+     */
+    private $summary;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getSummary() {
+        return $this->summary;
+    }
+
+    public function setContent($summary) {
+        $this->content = $content;
+    }
+}
